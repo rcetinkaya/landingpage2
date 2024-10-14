41ebf227e38fca1c057669d2c02e3b8002a49322
Ramazan ÇETİNKAYA <rmzncetinkaya@outlook.com>
2024-10-14 14:28:23 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,14 @@ import { CoinCards } from "@/components/coinCards";
 import Divider from "@/components/divider";
 import DividerReverse from "@/components/dividerReverse";
 import { BestOfTheBest } from "@/components/homePage/bestOfTheBest";
-import { Collection } from "@/components/homePage/collection";
-import { LoveUs } from "@/components/homePage/loveUs";
 import { Collectible } from "@/components/homePage/sectionOne";
 import { WhyJoin } from "@/components/homePage/whyJoin";
-import { getServerAuthSession } from "@/server/auth";
-import { api, HydrateClient } from "@/trpc/server";
+import {  HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
 
-  void api.post.getLatest.prefetch();
 
-  return (
+  return ( 
     <HydrateClient>
       <main className="flex w-full min-h-screen flex-col items-center justify-center bg-black ">
         <div className="mb-[3%]">
